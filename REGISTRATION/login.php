de8e476e7047b7d06082d33b5cacea43a1900cce
shivam210811 <158142067+shivam210811@users.noreply.github.com>
2024-12-10 06:49:01 -0800

    Add files via upload

---
+++ REGISTRATION/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registration</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+        <div class="clg-lg-6">
+            <h2> registration form</h2>
+            <form action="validation.php" method="post">
+                <div class="forum-group">
+                    <label>username</label>
+                   <inpu<t type="text" name="names" class="form-control">
+                   <inpu<t type="password" name="password" class="form-control">
+                    <button class="btn btn-primary">login</button>
+            </form>
+        </div>
+<div class="clg-lg-6">
+            <h2> registration form</h2>
+            <form action="validation.php" method="post">
+                <div class="forum-group">
+                    <label>username</label>
+                   <inpu<t type="text" name="names" class="form-control">
+                   <inpu<t type="password" name="password" class="form-control">
+                    <button class="btn btn-primary">login</button>
+            </form>
+</div>
+                </div>
+  </div>
+
+</body>
+</html>
